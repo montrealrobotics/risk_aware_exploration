1f2f90215133d04020482dc14e893f1e9b0010d1
Kaustubh Mani <kaustubh3095@gmail.com>
2023-02-23 03:07:31 -0500

    minor changes

--- scripts/ppo_continuous_action.py
+++ scripts/ppo_continuous_action.py
@@ -5,9 +5,9 @@ import random
 import time
 from distutils.util import strtobool
 
-import gym
+import safety_gym, gym
 import numpy as np
-import pybullet_envs  # noqa
+#import pybullet_envs  # noqa
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -70,6 +70,8 @@ def parse_args():
         help="the maximum norm for the gradient clipping")
     parser.add_argument("--target-kl", type=float, default=None,
         help="the target KL divergence threshold")
+    parser.add_argument("--model_name", type=str, default="ppo", 
+        help="name of the model" ) 
     args = parser.parse_args()
     args.batch_size = int(args.num_envs * args.num_steps)
     args.minibatch_size = int(args.batch_size // args.num_minibatches)
@@ -81,9 +83,9 @@ def make_env(env_id, seed, idx, capture_video, run_name, gamma):
     def thunk():
         env = gym.make(env_id)
         env = gym.wrappers.RecordEpisodeStatistics(env)
-        if capture_video:
-            if idx == 0:
-                env = gym.wrappers.RecordVideo(env, f"videos/{run_name}")
+        #if capture_video:
+        #    if idx == 0:
+        #        env = gym.wrappers.RecordVideo(env, f"videos/{run_name}")
         env = gym.wrappers.ClipAction(env)
         env = gym.wrappers.NormalizeObservation(env)
         env = gym.wrappers.TransformObservation(env, lambda obs: np.clip(obs, -10, 10))
@@ -147,8 +149,8 @@ if __name__ == "__main__":
             sync_tensorboard=True,
             config=vars(args),
             name=run_name,
-            monitor_gym=True,
-            save_code=True,
+            #monitor_gym=True,
+            #save_code=True,
         )
     writer = SummaryWriter(f"runs/{run_name}")
     writer.add_text(
@@ -184,10 +186,10 @@ if __name__ == "__main__":
     # TRY NOT TO MODIFY: start the game
     global_step = 0
     start_time = time.time()
-    next_obs = torch.Tensor(envs.reset()).to(device)
+    next_obs = torch.Tensor(envs.reset()[0]).to(device)
     next_done = torch.zeros(args.num_envs).to(device)
     num_updates = args.total_timesteps // args.batch_size
-
+    return_, cum_cost, ep_cost = 0.0, np.array([0.]), np.array([0.])
     for update in range(1, num_updates + 1):
         # Annealing the rate if instructed to do so.
         if args.anneal_lr:
@@ -208,16 +210,29 @@ if __name__ == "__main__":
             logprobs[step] = logprob
 
             # TRY NOT TO MODIFY: execute the game and log data.
-            next_obs, reward, done, info = envs.step(action.cpu().numpy())
+            next_obs, reward, done, dummy, info = envs.step(action.cpu().numpy())
+            
             rewards[step] = torch.tensor(reward).to(device).view(-1)
             next_obs, next_done = torch.Tensor(next_obs).to(device), torch.Tensor(done).to(device)
-
-            for item in info:
-                if "episode" in item.keys():
-                    print(f"global_step={global_step}, episodic_return={item['episode']['r']}")
-                    writer.add_scalar("charts/episodic_return", item["episode"]["r"], global_step)
-                    writer.add_scalar("charts/episodic_length", item["episode"]["l"], global_step)
-                    break
+            return_ += args.gamma * reward 
+            try:
+                ep_cost += info["cost"]; cum_cost += info["cost"]
+            except:
+                pass
+            if done:
+                wandb.log({"episodic_return": return_}, step=global_step)
+                wandb.log({"episodic_cost": ep_cost}, step=global_step)
+                wandb.log({"cummulative_cost": cum_cost}, step=global_step)
+                print(f"global_step={global_step}, episodic_return={return_}")
+                return_ = 0
+                ep_cost = np.array([0.])
+                
+            #for item in info:
+            #    if "episode" in item.keys():
+            #        print(f"global_step={global_step}, episodic_return={item['episode']['r']}")
+            #        writer.add_scalar("charts/episodic_return", item["episode"]["r"], global_step)
+            #        writer.add_scalar("charts/episodic_length", item["episode"]["l"], global_step)
+            #        break
 
         # bootstrap value if not done
         with torch.no_grad():
